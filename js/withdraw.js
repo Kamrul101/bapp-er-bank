46a37bc902e97d654ba653779d4524751d3aa0be
Kamrul101 <ridoyhasan87@gmail.com>
2023-02-15 13:49:30 +0600

    update

--- js/withdraw.js
+++ js/withdraw.js
@@ -6,24 +6,37 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const withdrawAmount = withdrawField.value;
     
+    withdrawField.value ='';
+
+    if(isNaN(withdrawAmount)){
+        alert('provide Valid amount');
+        return;
+    }
     //previous withdraw amount
     const preWithdrawAmount = document.getElementById('update-withdraw-amount');
     const preWithdrawTotal = preWithdrawAmount.innerText;
-    
+   
 
-    const newWithdrawTotal = parseFloat(withdrawAmount) + parseFloat(preWithdrawTotal);
-
-    //set total withdraw to withdraw section
-    preWithdrawAmount.innerText = newWithdrawTotal;
 
     const balanceTotal = document.getElementById('balance-total');
     const preBalance = balanceTotal.innerText;
+    
+    
+    
+    if(preBalance < withdrawAmount){
+        alert("Don't have enough money" );
+        return;
+    }
+    const newWithdrawTotal = parseFloat(withdrawAmount) + parseFloat(preWithdrawTotal);
+    
 
+    //set total withdraw to withdraw section
+    preWithdrawAmount.innerText = newWithdrawTotal;
     //new balance
     const newBalance = parseFloat(preBalance) - parseFloat(newWithdrawTotal);
 
     //set new balance
     balanceTotal.innerText = newBalance;
 
-    withdrawField.value ='';
+    
 })
